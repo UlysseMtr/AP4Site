1b594a8039df10af5c6d271d3e7f8f0b9ab769eb
Ulysse <ulyssemetra@gmail.com>
2024-03-12 22:34:01 +0100

    configuration de la page de connexion en php + token

--- choix.php
+++ choix.php
@@ -8,10 +8,34 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
 </head>
 <body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+
+$connexion = new PDO("mysql:host=$servername;dbname=M2L", $username, $password);
+
+$id = $_COOKIE['id'];
+$token = $_COOKIE['token'];
+
+if ($token){
+    $req = $connexion->query("SELECT * FROM compteadmin WHERE id = '$id' AND token = '$token'");
+    $rep = $req->fetch();
+
+    if ($rep['pseudo'] != false){
+        echo "Vous êtes bien connecté ".$rep['pseudo']." !";
+    }
+}else{
+    header("Location: index.php");
+}
+?>
     <form>
      
         <h1>M2L CROSL</h1>
         <div align="center">
+            <?php
+            echo "Vous êtes bien connecté !"
+            ?>
             <button type="submit" formaction="creeoumodif.html" formmethod="post">Ligue</button>
             <button type="submit" formaction="creeoumodifpresta.html" formmethod="post">Prestation</button>
             <button type="submit" formaction="choixfacture.html" formmethod="post">Facture</button>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CROSL</title>
+    <link rel="stylesheet" href="css/creeligue.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+</head>
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "password";
+
+    $connexion = new PDO("mysql:host=$servername;dbname=M2L", $username, $password);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            $id = $_POST['id'];
+            $mdp = $_POST['mdp'];
+            // Vérifiez que les données rentrées ne sont pas vide
+            if ($id != "" && $mdp != ""){
+                // Création d'un token aléatoire
+                $token = bin2hex((random_bytes(32)));
+
+                // Connexion à la base de donnée
+                $req = $connexion->query("SELECT * FROM compteadmin WHERE id = '$id' AND mdp = '$mdp'");
+                $req = $req->fetch();
+                if ($req['id'] != false){
+                    $connexion->exec("UPDATE compteadmin SET token = '$token' WHERE id = '$id' AND mdp = '$mdp'");
+                    setcookie("token", $token, time() + 3600);
+                    setcookie("id", $id, time() + 3600);
+                    // c'est bon --> redirection vers une autre page
+                    header("Location: choix.php");
+                    exit();
+                }else{
+                    // c'est pas bon
+                    $error_msg = "Identifiant ou mdp incorrect";
+                }
+            }
+        }
+    ?>
+<form method="POST" action="">
+
+    <h1>Page Admin</h1>
+    <div class="inputs">
+        <input type="text" name="id" placeholder="Identifiant" />
+        <input type="password" name="mdp" placeholder="Mot de passe">
+    </div>
+    <div align="center">
+         <button type="submit" name="envoi">Se connecter</button>
+    </div>
+</form>
+
+<?php
+if ($error_msg){
+    ?>
+    <p><?php echo $error_msg; ?></p>
+    <?php
+}
+?>
+</body>
+</html>
